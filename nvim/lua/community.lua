9d000da22aa041fe44f352d211201b82f4bdbba8
Gabriel Sampson <g@samp.sn>
2024-05-24 15:56:14 +0000

    Update nvim config

--- nvim/lua/community.lua
+++ nvim/lua/community.lua
@@ -8,13 +8,13 @@ return {
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.python-ruff" },
   { import = "astrocommunity.pack.go" },
-  { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.rust" },
   { import = "astrocommunity.color.transparent-nvim" },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
   { import = "astrocommunity.split-and-window.windows-nvim" },
   { import = "astrocommunity.color.tint-nvim" },
   { import = "astrocommunity.file-explorer.oil-nvim" },
+  { import = "astrocommunity.motion.nvim-surround" },
   -- { import = "astrocommunity.test.neotest" },
   -- import/override with your plugins folder
 }
